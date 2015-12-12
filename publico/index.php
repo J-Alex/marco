afa8f0347883f7ff01b50ac5ec05edf1c080c9d7
J-Alex <jlex.wd@outlook.com>
2015-12-11 18:28:41 -0600

    publico

---
+++ publico/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>{WebDev}</title>
+	<link rel="stylesheet" href="/res/css/styles.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<header class="fullScreen">
+	<h1>Web Developer</h1>
+</header>
+<nav></nav>
+<main></main>
+<footer></footer>
+<script src="/res/js/scripts.min.js"></script>
+</body>
+</html>
+
